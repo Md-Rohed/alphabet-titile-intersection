7ca7df46577efbb949c14f3eb4670366050e4b7d
Rohed-Appnap <rohed@appnap.io>
2024-05-22 18:45:48 +0600

    Alphabet Tile Interaction (Task-2)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const AlphabetTiles = () => {
+  const [outputString, setOutputString] = useState("");
+
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const splitLetterArr = letters.split("");
+
+  const handleTileClick = (letter) => {
+    let newOutputString = outputString + letter;
+
+    const regex = /(.)\1{2,}/g;
+    newOutputString = newOutputString.replace(regex, (match) =>
+      "_".repeat(Math.ceil(match.length / 3))
+    );
+
+    setOutputString(newOutputString);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-center mt-[5rem] text-[2.5rem] font-bold text-blue-500">
+        Alphabet Tile Interaction
+      </h1>
+      <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-4 p-4 px-[5rem] ">
+        {splitLetterArr.map((letter) => (
+          <button
+            role="button"
+            key={letter}
+            className="tile bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded shadow-lg"
+            onClick={() => handleTileClick(letter)}
+          >
+            {letter}
+          </button>
+        ))}
+      </div>
+      <div className="mt-4 p-4 flex items-center justify-center ">
+        {outputString ? (
+          <div className="font-bold text-[2rem]">{outputString}</div>
+        ) : (
+          <p className="font-bold text-[2rem]">Click alphabet to find result</p>
+        )}
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default AlphabetTiles;
